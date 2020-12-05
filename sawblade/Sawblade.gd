46b61c39e5a2821b6e0a67257b416d48b33344a9
lampperson <jamesevora367@gmail.com>
2020-12-05 00:59:57 -0800

    commit_12/5/2020

--- sawblade/Sawblade.gd
+++ sawblade/Sawblade.gd
@@ -1,28 +1,19 @@
 extends KinematicBody2D
-
-const UP = Vector2(0,-1)
-const GRAVITY = 20
+const RETURN = Vector2(0,0)
 const SPEED = 200
-const JUMP_HEIGHT = -550
 
+var changedirectionbuffer = 0
+var direction = Vector2(1,0)
 var motion = Vector2()
 
 func _physics_process(delta):
-	motion.y += 10
-	
-	if Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
-	
-	elif Input.is_action_pressed("ui_left"):
-		motion.x = - SPEED
-	
-	else:
-		motion.x = 0
-	
-	if is_on_floor():
-		print("On floor.")
-		if Input.is_action_just_pressed("ui_up"):
-			motion.y = JUMP_HEIGHT
-			
-	motion = move_and_slide(motion, UP)
-	pass
+	motion = direction * SPEED
+	$RayCast2D.enabled = true
+	$RayCast2D.cast_to = direction * 20
+	changedirectionbuffer-=1
+	if $RayCast2D.is_colliding() and changedirectionbuffer < 0:
+		changedirectionbuffer = 10
+		direction.x *= -1
+		direction.y *= -1
+		print("String changing direction")
+	motion = move_and_slide(motion)
