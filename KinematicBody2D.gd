552236c983a3e83111895673abdfdfd1c21e8060
hannahw1004 <hannah.wee.2004@gmail.com>
2020-12-04 22:21:10 -0800

    12/4/2020

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,28 +1,12 @@
 extends KinematicBody2D
-
-
-const UP = Vector2(0,-1)
-const GRAVITY = 20
 const SPEED = 200
 
-
+var direction = Vector2(1,0)
 var motion = Vector2()
 
 func _physics_process(delta):
-	motion.y += 10
+	motion = direction * SPEED
+	$RayCast2D.cast_to = direction * 10
 	
-	if Input.is_on_wall("ui_right"):
-		motion.x = SPEED
-		$Sprite.flip_h = false;
-		$Sprite.play("Run")
-	elif Input.is_on_wall("ui_left"):
-		motion.x = - SPEED
-		$Sprite.flip_h = true;
-		$Sprite.play("Run")
-	else:
-		motion.x = 0
-	
-			
 	motion = move_and_slide(motion)
 	pass
-
