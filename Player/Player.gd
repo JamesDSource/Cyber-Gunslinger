8986b3674d06a65948d0965bd7423bcfbcc20fdd
jwjung-04 <72804357+jwjung-04@users.noreply.github.com>
2020-12-04 22:12:15 -0800

    player animations

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,8 +1,11 @@
 extends KinematicBody2D
 
 const GRAVITY = 10
-const SPEED = 100
+const SPEED = 200
 const JUMP = -400
+
+var animation = "Idle"
+var animationSet = ""
 
 var motion = Vector2()
 
@@ -10,19 +13,34 @@ func _ready():
 	pass
 
 func _process(delta):
-	pass
+	if animation != animationSet:
+		$AnimationPlayer.play(animation)
+		animationSet = animation
 
 func _physics_process(delta):
 	motion.y += GRAVITY
 	
 	if Input.is_action_pressed("Left"):
 		motion.x = -SPEED
+		$Sprite.flip_h = true
 	elif Input.is_action_pressed("Right"):
 		motion.x = SPEED
+		$Sprite.flip_h = false
 	else:
 		motion.x = 0
 	
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		motion.y = JUMP
+	if is_on_floor():
+		if motion.x == 0:
+			animation = "Idle"
+		else:
+			animation = "Run"
+
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = JUMP
+	else:
+		if motion.y > 0:
+			animation = "Fall"
+		else:
+			animation = "Jump"
 	
 	motion = move_and_slide(motion, Vector2.UP)
