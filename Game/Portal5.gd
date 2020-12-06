1789aee2861656fa005baac7fd182e7af447104e
jwjung-04 <72804357+jwjung-04@users.noreply.github.com>
2020-12-05 19:21:01 -0800

    levels

--- Game/Portal5.gd
+++ Game/Portal5.gd
@@ -4,4 +4,4 @@ func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
 		if body.name == "Player":
-			get_tree().change_scene("res://titlescreen/MainScreen.tscn")
+			get_tree().change_scene("res://finish/congrats.tscn")
