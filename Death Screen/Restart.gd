76c7da62e85177fcaba43ce21d0af3fa3e24c9ed
lampperson <jamesevora367@gmail.com>
2020-12-05 19:23:44 -0800

    commit_12/5/2020

--- Death Screen/Restart.gd
+++ Death Screen/Restart.gd
@@ -1,18 +1,6 @@
-extends Button
-
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
+extends Node2D
+var player_dead = false
+func _process(delta):
+	visible = true if!(get_tree().paused) && player_dead else false
 func _on_Restart_pressed():
-	get_tree().change_sceen("res://Game/Game.tscn")
-	pass
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	get_tree().reload_current_scene()
