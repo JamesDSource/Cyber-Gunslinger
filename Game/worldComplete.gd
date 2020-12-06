09f51fe047c25550873c9724a09afe0dd4616f04
hannahw1004 <hannah.wee.2004@gmail.com>
2020-12-05 18:02:04 -0800

    12/5/2020

--- Game/worldComplete.gd
+++ Game/worldComplete.gd
@@ -5,5 +5,5 @@ func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
 		if body.name == "Player":
-			get_tree().change_scene(".tscn")
+			get_tree().change_scene("Level4.tscn")
 		
